ead2af2e131e82ea8c60c5a73b6ad2efa04917f1
201-12 <201-12@201-12-PC>
2018-03-08 11:45:15 +0900

    [이춘림] index.jsp위치 수정 컨트롤러 내용수정

--- mvc27team4/src/IndexController.java
+++ mvc27team4/src/IndexController.java
@@ -15,7 +15,7 @@ public class IndexController extends HttpServlet {
 	
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("WEB-INF/views/index.jsp").forward(request, response);
 	}
 
 	
