583410c989fce7ba836138d1602d10d2539a052f
201-12 <201-12@201-12-PC>
2018-03-09 14:41:35 +0900

    [이춘림] DriverDB.java/ driverConnection()메서드 static메서드로 변경

--- mvc27team4/src/model/DriverDB.java
+++ mvc27team4/src/model/DriverDB.java
@@ -5,17 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DriverDB {
-	Connection connection = null;
 	
-	public Connection driverConnection() throws ClassNotFoundException, SQLException {
-		
+	public static Connection driverConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
 		
 		String url = "jdbc:mysql://localhost:3306/mvc?useUnicode=true&characterEncoding=utf8";
 		String id = "root";
 		String pw = "java0000";
-		
-		connection = DriverManager.getConnection(url, id, pw);
+		Connection connection = DriverManager.getConnection(url, id, pw);
 		
 		return connection;
 	}
