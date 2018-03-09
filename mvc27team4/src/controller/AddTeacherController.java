234cb48a4beef25fdf4b632d4d2d4e7856dbaf13
201-12 <201-12@201-12-PC>
2018-03-09 12:08:39 +0900

    [이춘림] 1. DB insert 되는 것 확인.(addTeacher.jsp, AddTeacherController.java 수정) 2. WEB-INF/include 폴더 추가 한 후, employee, student, teacher 폴더 나누고 .jsp파일 만들어둠.

--- mvc27team4/src/controller/AddTeacherController.java
+++ mvc27team4/src/controller/AddTeacherController.java
@@ -8,11 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.Teacher;
+import service.TeacherDao;
+
 
 @WebServlet("/addTeacher.jjdev")
 public class AddTeacherController extends HttpServlet {
 	
-	/*private StudentDao studentDao;*/
+	private TeacherDao teacherDao;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/views/teacher/addTeacher.jsp").forward(request, response);
@@ -24,16 +27,22 @@ public class AddTeacherController extends HttpServlet {
 		//2. 모델(DAO) 호출
 		//3. list redirect
 		
-		String teacherId = request.getParameter("guestId");
+		//DB접근하는 메서드가 있는 .java파일에서 setCharacterEncoding을 UTF-8로 설정하면 DB인서트에 한글이 된다.
+		request.setCharacterEncoding("UTF-8");
+		
+		String teacherId = request.getParameter("teacherId");
 		String teacherPw = request.getParameter("teacherPw");
 		
-		/*Teacher teahcer = new Teacher();
-		TeacherDao teacherDao = new TeacherDao();
-		teacherDao.insertGuest(Guest);*/
+		Teacher teacher = new Teacher();
+		teacher.setTeacherId(teacherId);
+		teacher.setTeacherPw(teacherPw);
+		
+		this.teacherDao = new TeacherDao();
+		this.teacherDao.insertTeacher(teacher);
 		
 		//나는 뷰가 없으므로 리스트쪽을 재용청하겠다.
-		//뷰를 가진것 리다이렉트
-		response.sendRedirect("/getTeacherList.jjdev");
+		//뷰를 가진것으로 리다이렉트
+		response.sendRedirect(request.getContextPath() + "/getTeacherList.jjdev");
 	}
 
 }
