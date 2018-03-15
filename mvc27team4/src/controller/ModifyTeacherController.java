ff718327e973e09c3b8b319f0b2ba3af67458201
201-12 <201-12@201-12-PC>
2018-03-15 11:16:57 +0900

    [이춘림] ModifyTeacherController.java 어노테이션 경로탈자로 인해 톰캣 애러 수정

--- mvc27team4/src/controller/ModifyTeacherController.java
+++ mvc27team4/src/controller/ModifyTeacherController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("modifyTeacher.jjdev")
+@WebServlet("/modifyTeacher.jjdev")
 public class ModifyTeacherController extends HttpServlet {
 	
 	public ModifyTeacherController() {
