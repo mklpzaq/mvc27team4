618023bc03c0a2c10066a513ceef5949cc182c2e
guest_0 <guest_0@220.87.173.127>
2018-03-22 07:17:29 +0900

    [이춘림] AddTeacherAddrController.java에 대한 주석 추가

--- mvc27team4/src/controller/AddTeacherAddrController.java
+++ mvc27team4/src/controller/AddTeacherAddrController.java
@@ -17,7 +17,6 @@ import model.TeacherAddrDao;
 public class AddTeacherAddrController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("doGet AddTeacherAddrController.java");
-		
 		request.getRequestDispatcher("/WEB-INF/views/teacher/addTeacherAddrForm.jsp").forward(request, response);
 	}
 
@@ -36,6 +35,17 @@ public class AddTeacherAddrController extends HttpServlet {
 		TeacherAddrDao teacherAdderDao = new TeacherAddrDao();
 		int result = teacherAdderDao.insertTeacherAddr(teacherAddr);
 		System.out.println("result : " + result);
+		/*
+		 * insertTeacherAddr() 메서드의 리턴값이 2라는 의미는
+		 * 해당 Teacher의 주소록의 개수가 5개가 초과되었다는 의미이다.
+		 * 따라서 주소록 개수 5개 초과에 따른 적절한 경고창을 보여주도록 if문을 사용하였다.
+		 * 
+		 * out.println() 메서드를 통해 인터넷 창에 글자를 적어
+		 * return ; (리턴 세미콜론)을 사용하면 새로운 html창이 뜨고 
+		 * out.pirntln() 메서드 매개변수로 넣었던 String형 글자가 html형식으로 읽히게 된다.
+		 * 그리고 경고창이 발생하고 확인을 누르면,
+		 *  location.href='./getTeacherAddrList.jjdev' 로 설정하였던 URL로 이동하게 된다.
+		 *  */
 		if(result == 2) {
 			response.setContentType("text/html;charset=UTF-8");
 			PrintWriter out = response.getWriter();
@@ -46,9 +56,6 @@ public class AddTeacherAddrController extends HttpServlet {
 			out.close();
 			return ;
 		}
-		
 		response.sendRedirect(request.getContextPath() + "/getTeacherAddrList.jjdev?teacherNo=" + teacherNo);
 	}
-	
-
 }
